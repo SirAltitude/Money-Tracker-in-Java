6d93a661834363d2d8bcbbc9f64b517303d16885
SirAltitude <74191280+SirAltitude@users.noreply.github.com>
2020-12-16 18:05:47 +0100

    VERSION WITH ENHANCED GRAPH CLASS

--- src/main/java/jframe/panels/GraphPanel.java
+++ src/main/java/jframe/panels/GraphPanel.java
@@ -1,7 +1,6 @@
 package jframe.panels;
 
 import controller.RegisterController;
-import jframe.BarGraph;
 import jframe.ChartPanel;
 import person.Person;
 
@@ -45,39 +44,5 @@ public class GraphPanel extends JPanel {
         }
         f.getContentPane().add(new ChartPanel(values,names,"Bar Chart"));
         f.setVisible(true);
-
-
-
-//        JFrame frameGraph = new JFrame("Bar chart");
-//        frameGraph.setSize(500,500);
-//        BarGraph graph = new BarGraph(this.controller);
-//        for(Person person: controller.getPeopleDB().getList())
-//        {
-//            int value = (int)((person.getTotalDebt())-controller.getPeopleDB().getSmallestDebt());
-//            if(value==0) {
-//                if(graph.getBars().containsKey(Color.BLACK))
-//                graph.addBar(Color.cyan,1);
-//                else graph.addBar(Color.black,1);
-//            }
-//            else if(graph.getBars().containsKey(Color.GREEN))
-//            {
-//                graph.addBar(Color.RED,value);
-//            }
-//            else if(graph.getBars().containsKey(Color.BLUE))
-//            {
-//                graph.addBar(Color.GREEN,value);
-//            }
-//            else if(graph.getBars().containsKey(Color.YELLOW)) {
-//                graph.addBar(Color.BLUE, value);
-//            }
-//            else if(graph.getBars().containsKey(Color.GRAY)) {
-//                graph.addBar(Color.YELLOW, value);
-//            }
-//            else graph.addBar(Color.GRAY,value);
-//        }
-//
-//        frameGraph.getContentPane().add(graph);
-//        frameGraph.setVisible(true);
-
     }
 }
